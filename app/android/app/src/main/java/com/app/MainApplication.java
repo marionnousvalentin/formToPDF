c88a9eea9328118f6dc0a20fa8ccbb4e14e96a7c
Marion Barré <marionb@bam.tech>
2019-01-31 18:34:38 +0100

    install pdf

--- app/android/app/src/main/java/com/app/MainApplication.java
+++ app/android/app/src/main/java/com/app/MainApplication.java
@@ -3,6 +3,8 @@ package com.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.wonday.pdf.RCTPdfView;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTPdfView(),
+            new RNFetchBlobPackage(),
             new RNHTMLtoPDFPackage()
       );
     }
